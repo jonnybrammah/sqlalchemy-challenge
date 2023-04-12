36c4f768eb053952be64f83b0a166f390b0f81bc
Jonny Brammah <jonny.brammah@gmail.com>
2023-04-12 16:10:55 -0700

    Finished climate_starter.ipynb (needs tweaks) and started work on app.py

--- app.py
+++ app.py
@@ -1,30 +1,75 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Retreive last 12 months of precipitation data
+
+    #Set a variable to store the date of one year ago
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    #Query all dates and precipitation data from a year ago until now
+    last_years_prcp = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date > year_ago).\
+        order_by(Measurement.date).all()
+    
+    #Create a dictionary from the row data and append to a list of precipitation_data
+    precipitation_data = []
+    for date, prcp in last_years_prcp:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        last_years_prcp.append(prcp_dict)
+    
+    return jsonify(precipitation_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+session.close()
