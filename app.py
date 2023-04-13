d8f7c4abd6e85b4b4007516c9572070b6bca158a
Jonny Brammah <jonny.brammah@gmail.com>
2023-04-13 16:49:04 -0700

    Updated app.py to include API routes for temperature with start and end dates

--- app.py
+++ app.py
@@ -39,37 +39,207 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"<h1>Welcome to the Surf's Up Weather API</h2>"
+        f"<p>Available Routes:</p>"
+        "<ul>"
+        f"<li> <strong>Precipitation Data:</strong> /api/v1.0/precipitation<br/> </li>"
+        f"<li> <strong>List all Stations:</strong> /api/v1.0/stations<br/> </li>"
+        f"<li> <strong>Observed Temperature Data from Most Active Station:</strong> /api/v1.0/tobs<br/></li>"
+        f"<li> <strong>Max, min, and average temperatures since specified start date:</strong> /api/v1.0/yyyy-mm-dd<br/></li>"
+        f"<li> <strong>Max, min, and average temperatures between specified start date and end date (List start date first):</strong>/api/v1.0/yyyy-mm-dd/yyyy-mm-dd<br/></li>"
+        "</ul>"
     )
 
+########################################################################################################
+#Return the previous 12 months of precipitation data as a JSON list
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Retreive last 12 months of precipitation data
+    #Retreive last 12 months of precipitation data
 
     #Set a variable to store the date of one year ago
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
 
     #Query all dates and precipitation data from a year ago until now
     last_years_prcp = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date > year_ago).\
         order_by(Measurement.date).all()
     
+    #Close the session after the query is complete
+    session.close()
+
     #Create a dictionary from the row data and append to a list of precipitation_data
     precipitation_data = []
     for date, prcp in last_years_prcp:
         prcp_dict = {}
-        prcp_dict["date"] = date
-        prcp_dict["prcp"] = prcp
-        last_years_prcp.append(prcp_dict)
-    
+        #Set the date as the key and the value of prcp as the result
+        prcp_dict[date] = prcp
+        precipitation_data.append(prcp_dict)
+
+    #Return a jsonified version of our dictionary
     return jsonify(precipitation_data)
 
+########################################################################################################
+#Return a JSON list of all stations from the dataset
+@app.route("/api/v1.0/stations")
+def stations():
+
+    #Query the names of all stations
+    station_list = session.query(Station.station, Station.name).all()
+    #Close the session after the query is complete
+    session.close()
+    #Create a dictionary from the row data and append to a list of station_data
+    station_data = []
+
+    for station, name in station_list:
+        station_dict = {}
+        station_dict["name"] = name
+        station_data.append(station_dict)
+
+    #Return a jsonified version of our dictionary
+    return jsonify(station_data)
+
+########################################################################################################
+#Return a JSON list of temperature observations of the most-active station for the previous year of data
+@app.route("/api/v1.0/tobs")
+def tobs_func():
+    #Retreive last 12 months of temperature data
+
+    #Set a variable to store the date of one year ago
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the date and temperature data
+    USC00519281_last_years_temp = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date > year_ago).\
+        filter(Measurement.station == "USC00519281").\
+        order_by(Measurement.date).all()
+    
+    #Close the session after the query is complete
+    session.close()
+
+    #Create a dictionary from the row data and append to a list of tobs_data
+    tobs_data = []
+    for date, tobs in USC00519281_last_years_temp:
+        tobs_dict = {}
+
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+
+        tobs_data.append(tobs_dict)
+
+    #Return a jsonified version of our dictionary
+    return jsonify(tobs_data)
+
+########################################################################################################
+#Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start date
+@app.route("/api/v1.0/<start>")
+def minmaxmean_temps(start):
+    lowest_temperature = session.query(Measurement.station , func.min(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+    highest_temperature = session.query(Measurement.station , func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+    average_temperature = session.query(Measurement.station , func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+
+    #Close the session after the query is complete
+    session.close()
+
+#My indepdnent work that worked but was inelegant:
+    # tobs_values = []
+
+    # for station, min in lowest_temperature:
+    #     min_temp_dict = {}
+    #     min_temp_dict["tmin"] = min
+    #     tobs_values.append(min_temp_dict)
+    # print("min temp added")
+    # for station, max in highest_temperature:
+    #     max_temp_dict = {}
+    #     max_temp_dict["tmax"] = max
+    #     tobs_values.append(max_temp_dict)
+    # print("max temp added")
+    # for station, average in average_temperature:
+    #     avg_temp_dict = {}
+    #     avg_temp_dict["tavg"] = average
+    #     tobs_values.append(avg_temp_dict)
+    #    return jsonify(tobs_values)
+
+    # Create a dictionary to store the temperature values
+    tobs_values = {}
+    # Convert the row objects to dictionaries
+    lowest_temp_dict = {"tmin": lowest_temperature[0][1]}
+    avg_temp_dict = {"tavg": average_temperature[0][1]}
+    highest_temp_dict = {"tmax": highest_temperature[0][1]}
+    # Add the dictionaries to the tobs_values dictionary
+    tobs_values.update(lowest_temp_dict)
+    tobs_values.update(avg_temp_dict)
+    tobs_values.update(highest_temp_dict)
+    # Return the dictionary as a JSON response
+    return jsonify(tobs_values)
+
+
+
+
+########################################################################################################
+#Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start date
+@app.route("/api/v1.0/<start>/<end>")
+def minmaxmean_temps_start_and_end(start, end):
+    lowest_temperature = session.query(Measurement.station , func.min(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+    highest_temperature = session.query(Measurement.station , func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+    average_temperature = session.query(Measurement.station , func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+
+    #Close the session after the query is complete
+    session.close()
+
+    ## My independent work that was a little inelegant:
+    # tobs_values = []
+
+    # for station, min in lowest_temperature:
+    #     min_temp_dict = {}
+    #     min_temp_dict["tmin"] = min
+    #     tobs_values.append(min_temp_dict)
+    # print("min temp added")
+    # for station, max in highest_temperature:
+    #     max_temp_dict = {}
+    #     max_temp_dict["tmax"] = max
+    #     tobs_values.append(max_temp_dict)
+    # print("max temp added")
+    # for station, average in average_temperature:
+    #     avg_temp_dict = {}
+    #     avg_temp_dict["tavg"] = average
+    #     tobs_values.append(avg_temp_dict)
+    # return jsonify(tobs_values)
+
+    ## Work completed with the help of askBCS
+    # Create a dictionary to store the temperature values
+    tobs_values = {}
+    # Convert the row objects to dictionaries
+    lowest_temp_dict = {"tmin": lowest_temperature[0][1]}
+    avg_temp_dict = {"tavg": average_temperature[0][1]}
+    highest_temp_dict = {"tmax": highest_temperature[0][1]}
+    # Add the dictionaries to the tobs_values dictionary
+    tobs_values.update(lowest_temp_dict)
+    tobs_values.update(avg_temp_dict)
+    tobs_values.update(highest_temp_dict)
+    # Return the dictionary as a JSON response
+    return jsonify(tobs_values)
+
+
+
+
+
+################################################################################
+#Jonny, you moron, do not delete this line of code.
 if __name__ == '__main__':
     app.run(debug=True)
-
-session.close()
